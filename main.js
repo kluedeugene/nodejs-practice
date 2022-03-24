01942de8f073afe28a6b61367635e6c23b31297e
김유진 <50358936+kluedeugene@users.noreply.github.com>
2022-03-24 13:30:26 +0900

    글수정-파일명 변경,내용저장

--- main.js
+++ main.js
@@ -118,14 +118,14 @@ var app = http.createServer(function (request, response) {
 					`
 					<form action= "/update_process" method="post">
 					<input type="hidden" name= "id" value="${title}">
-			<p><input type="text" name="title" placeholder="title" value=${title}></p>
-            <p>
-              <textarea name="description" placeholder="description">${description}</textarea>
-            </p>
-            <p>
-              <input type="submit">
-            </p>
-          </form>
+					<p><input type="text" name="title" placeholder="title" value=${title}></p>
+        		   <p>
+          		    <textarea name="description" placeholder="description">${description}</textarea>
+        		    </p>
+       	 		    <p>
+        	      <input type="submit">
+        		    </p>
+       			   </form>
 					`,
 
 					`<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
@@ -133,6 +133,27 @@ var app = http.createServer(function (request, response) {
 				response.writeHead(200);
 				response.end(template);
 			});
+		});
+	} else if (pathname === '/update_process') {
+		var body = '';
+		request.on('data', function (data) {
+			// callback.
+			body = body + data;
+		});
+		request.on('end', function () {
+			var post = qs.parse(body);
+			var title = post.title;
+			var description = post.description;
+			var id = post.id;
+
+			fs.rename(`data/${id}`, `data/${title}`, function (error) {
+				fs.writeFile(`data/${title}`, description, 'utf-8', function (err) {
+					response.writeHead(302, { Location: `/?id=${title}` });
+					response.end();
+				});
+			});
+
+			console.log(post);
 		});
 	} else {
 		response.writeHead(404);
