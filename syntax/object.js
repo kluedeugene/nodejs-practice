72ba936369c9872e7f27806a92f402624f409f75
김유진 <50358936+kluedeugene@users.noreply.github.com>
2022-03-25 14:17:58 +0900

    javascript 객체반복

--- syntax/object.js
+++ syntax/object.js
@@ -1,5 +1,10 @@
 var members = ['egoing', 'kimeugene', 'yaho']; //배열
 console.log(members[1]);
+var i = 0;
+while (i < members.length) {
+	console.log('array loop:', members[i]);
+	i = i + 1;
+}
 
 var roles = {
 	programmer: 'egoing',
@@ -7,4 +12,9 @@ var roles = {
 	manager: 'yaho'
 };
 
-console.log(roles.programmer);
+console.log(roles.programmer); //egoing
+console.log(roles['programmer']); //egoing
+
+for (var name in roles) {
+	console.log('object =>', name, 'value=>', roles[name]);
+}
