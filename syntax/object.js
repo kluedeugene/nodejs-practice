b8bc169b4ba6fe6c669a6b999b524eb23ac644a9
김유진 <50358936+kluedeugene@users.noreply.github.com>
2022-03-25 14:12:01 +0900

    js 객체의 형식

---
+++ syntax/object.js
@@ -0,0 +1,10 @@
+var members = ['egoing', 'kimeugene', 'yaho']; //배열
+console.log(members[1]);
+
+var roles = {
+	programmer: 'egoing',
+	designer: 'kimeugene',
+	manager: 'yaho'
+};
+
+console.log(roles.programmer);
