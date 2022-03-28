1cb1218d02b288feea8f11dff1ce03489a47679e
김유진 <50358936+kluedeugene@users.noreply.github.com>
2022-03-28 11:59:04 +0900

    template 모듈화(라이브러리화)

--- main.js
+++ main.js
@@ -2,67 +2,7 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
-
-var template = {
-	html: function (title, list, body, control) {
-		return `
-	  <!doctype html>
-	  <html>
-	  <head>
-		<title>WEB - ${title}</title>
-		<meta charset="utf-8">
-	  </head>
-	  <body>
-		<h1><a href="/">WEB</a></h1>
-	   ${list}
-	   ${control}
-	   ${body}
-	  </body>
-	  </html>
-	  `;
-	},
-	list: function (filelist) {
-		var list = '<ul>';
-
-		var i = 0;
-		while (i < filelist.length) {
-			list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-			i = i + 1;
-		}
-		list = list + '</ul>';
-		return list;
-	}
-};
-
-function templateHTML(title, list, body, control) {
-	return `
-  <!doctype html>
-  <html>
-  <head>
-    <title>WEB - ${title}</title>
-    <meta charset="utf-8">
-  </head>
-  <body>
-    <h1><a href="/">WEB</a></h1>
-   ${list}
-   ${control}
-   ${body}
-  </body>
-  </html>
-  `;
-}
-
-function templateList(filelist) {
-	var list = '<ul>';
-
-	var i = 0;
-	while (i < filelist.length) {
-		list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-		i = i + 1;
-	}
-	list = list + '</ul>';
-	return list;
-}
+var template = require('./lib/template.js');
 
 var app = http.createServer(function (request, response) {
 	//request 요청할때 웹브라우저가 보낸 정보, response 응답할때 우리가 웹브라우저에게 전송할 정보
